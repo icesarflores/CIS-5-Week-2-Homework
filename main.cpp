7a9f7fe0736ddcfd5edad0ea3e15bc042543bd88
Cesar Flores <14372814+icesarflores@users.noreply.github.com>
2026-09-07 21:53:25 +0000

    Updates

--- main.cpp
+++ main.cpp
@@ -7,18 +7,19 @@ int main() {
 using namespace std;
 
   // Block 1 — who you are (from Lab 2)
-  std::cout << "=== About me ===\n";
   cout << "=== About Me === \n";
-  cout << "Name: \n";
-  cout << "Interests: \n";
-  cout << "CIS-5 Goals: \n";
-  cout << "Tool Path: \n";
+  cout << "Name: Cesar Flores\n";
+  cout << "Interests: Technology, writing, movies, music, art, painting. \n";
+  cout << "CIS-5 Goals: I'm looking to become proficient in C++. I feel that by understanding coding structure and the process will help me with other coding languages.\n";
+  cout << "Tool Path: Github codespace and VSCode. Also, different PC's to better learn git commands such as commit, push, pull, etc. \n";
+  cout << "Fun Fact: I bought my first coding book, \"JavaScript for the World Wide Web: Visual QuickStart Guide, 3rd Edition in 1999\", to learn code." << endl;
+  cout << "\n";
 
   // Block 2 — this week's goals (from outcomes)
   cout << "=== Week 02 Goals === \n";
   cout << "I can point at include, main, a statement and a comment. \n";
-  cout << "I can compile \n";
-  cout << "I can fix the first compile error and rebuild." << endl;
+  cout << "I can compile my code.  \n";
+  cout << "I can fix the first compile error and rebuild. I continue to learn github terminal commands. " << endl << "\n";
 
   return 0;
 }
